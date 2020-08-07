5925c1f84383f23cb4bdaa1b6df37e6ca566a8c5
Thai Bui <buiducthaisdsu23@gmail.com>
2020-08-07 14:58:42 +0700

    File Added Successfully!

--- OrderCalculate/GreetingPanel.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class GreetingPanel extends JPanel {
-    private JLabel greeting;
-    public GreetingPanel(){
-        greeting=new JLabel("Welcome to TeTe Coffee!");
-        add(greeting);
-    }
-}
--- Schedule.java
+++
@@ -1,4 +0,0 @@
-public interface Schedule {
-    void AddToSchedule(Activity tmp);
-    void Generate(Activity tmp);
-}
